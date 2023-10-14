7e2d2993fde3fb0373315206b082cf6411fe3d61
shahriar hasan <shahriar.hasan.1523@gmail.com>
2023-10-15 02:33:47 +0600

    Some minor changes

--- dist/FakeStore.js
+++ dist/FakeStore.js
@@ -1,4 +1,4 @@
-class FakeStore {
+export class FakeStore {
   constructor(dbname) {
     this.dbname = dbname;
     if (!localStorage.getItem(dbname)) {
@@ -6,39 +6,46 @@ class FakeStore {
     }
   }
   get(key) {
-    if (key) {
-      let items = JSON.parse(localStorage.getItem(this.dbname));
-      return items.filter((item) => item[`_id`] == key);
-    } else {
-      return JSON.parse(localStorage.getItem(this.dbname));
+    try {
+      if (key) {
+        let items = JSON.parse(localStorage.getItem(this.dbname));
+        return items.filter((item) => item[`fs_id`] == key);
+      } else {
+        return JSON.parse(localStorage.getItem(this.dbname));
+      }
+    } catch (err) {
+      console.error(err);
     }
   }
-  push(key, item) {
-    let items = this.get();
-    if (!item) {
-      item = key;
-      key = this.createUID();
+  push(item, key) {
+    try {
+      let items = this.get();
+      if (!item) {
+        item = key;
+        key = this.createUID();
+      }
+      item.fs_id = key;
+      const exist = items?.find((it) => it.fs_id === key);
+      if (exist) {
+        throw new Error("Key already Exist");
+      }
+      items.push(item);
+      localStorage.setItem(this.dbname, JSON.stringify(items));
+      return this.get;
+    } catch (err) {
+      console.error(err);
     }
-    item._id = key;
-    const exist = items.find((it) => it._id === key);
-    if (exist) {
-      throw new Error("Key already Exist");
-      return;
-    }
-    items.push(item);
-    localStorage.setItem(this.dbname, JSON.stringify(items));
-    return this.get;
   }
   update(key, item) {
     let items = this.get();
     if (!key || !item) {
       throw new Error("Key or Updated Data missing");
     } else {
-      let index = items.findIndex((it) => it._id === key);
+      let index = items.findIndex((it) => it.fs_id === key);
       if (!index) {
         throw new Error("Key not matched");
       } else {
-        item._id = key;
+        item.fs_id = key;
         items[index] = item;
         localStorage.setItem(this.dbname, JSON.stringify(items));
         return this.get(key);
@@ -48,7 +55,7 @@ class FakeStore {
   remove(key) {
     let items = this.get();
     if (key) {
-      const restOfTheItems = items.filter((item) => item._id !== key);
+      const restOfTheItems = items.filter((item) => item.fs_id !== key);
       localStorage.setItem(this.dbname, JSON.stringify(restOfTheItems));
     } else {
       localStorage.setItem(this.dbname, JSON.stringify([]));
@@ -64,13 +71,4 @@ class FakeStore {
     end += "";
     return start + id + end;
   }
-}
-// Export the class for CommonJS environments
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = FakeStore;
-}
-
-// Export the class for ES6 environments
-if (typeof exports !== "undefined") {
-  exports.default = FakeStore;
 }
